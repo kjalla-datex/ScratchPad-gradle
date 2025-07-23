662bcfbe0210ada50869f55ff299c9134a459779
kjalla-datex <kjalla@datastealth.io>
2025-07-23 12:01:50 -0400

    RocksDBViewr

--- src/main/java/org/example/RocksDBViewer.java
+++ src/main/java/org/example/RocksDBViewer.java
@@ -1,14 +1,24 @@
+package org.example;
+
 import org.rocksdb.RocksDB;
 import org.rocksdb.Options;
+import org.rocksdb.RocksIterator;
 
 public class RocksDBViewer {
     public static void main(String[] args) throws Exception {
         RocksDB.loadLibrary();
         Options options = new Options().setCreateIfMissing(false);
-        try (RocksDB db = RocksDB.open(options, "/path/to/rocksdb-directory")) {
-            db.newIterator().forEachRemaining(entry -> {
-                System.out.println("Key: " + new String(entry.getKey()) + ", Value: " + new String(entry.getValue()));
-            });
+//        try (RocksDB db = RocksDB.open(options, "/path/to/rocksdb-directory")) {
+//        try (RocksDB db = RocksDB.open(options, "/Users/kjalla/IdeaProjects/nexus/data/repository/local/rocks")) {
+        try (RocksDB db = RocksDB.open(options, "../nexus/data/repository/local/rocks")) {
+            try(RocksIterator iterator = db.newIterator()) {
+                for(iterator.seekToFirst(); iterator.isValid(); iterator.next()) {
+                    System.out.println("Key: " + new String(iterator.key()) + ", Value: " + new String(iterator.value()));
+                }
+            }
+//            db.newIterator().forEachRemaining(entry -> {
+//                System.out.println("Key: " + new String(entry.getKey()) + ", Value: " + new String(entry.getValue()));
+//            });
         }
     }
 }
